3c5f7bb4d1067c0848bbc6c2210cf457d6342114
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2015-10-20 13:20:08 +1300

    Add bootstrap tooltip, google analytics

--- pdaf/scripts/init.js
+++ pdaf/scripts/init.js
@@ -2,7 +2,7 @@ requirejs.config({
     baseUrl: 'scripts/lib',
     paths: {
         app: '../app',
-        jquery: '/bower_components/jquery/dist/jquery',
+        jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
         d3: '//d3js.org/d3.v3.min'
     },
     waitSeconds: 15
