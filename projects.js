2bb4894b494987ddcb70b88fb4e204ca7f809930
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2015-11-04 11:59:55 +1300

    Add carousel

--- projects.js
+++ projects.js
@@ -6,17 +6,19 @@ app.controller('thumbs', ['$scope', '$http', function($scope, $http) {
 		image: null,
 		desc: null,
 		url: null,
+		prev: null,
+		next: null,
 		list: []
 	};
 
 
-	$scope.showPic = function(str, img, desc) {
-		console.log('clicked!', str, img);
+	$scope.showPic = function(str, img, desc, prev, next) {
 		$scope.data.style = 'show-it';
 		$scope.data.image = '/assets/images/' + img;
 		$scope.data.desc = desc;
 		$scope.data.url = str;
-		console.log($scope.data.style);
+		$scope.data.prev = prev;
+		$scope.data.next = next;
 	};
 
 	$scope.closeIt = function() {
@@ -24,11 +26,67 @@ app.controller('thumbs', ['$scope', '$http', function($scope, $http) {
 		$scope.data.image = null;
 		$scope.data.desc = null;
 		$scope.data.url = null;
-		console.log($scope.data.style);
-	}
+		$scope.data.prev = null;
+		$scope.data.next = null;
+	};
 
 	$http.get('projects.data.json').success(function(data) {
-		$scope.data.list = data.projects;
-		console.log($scope.data.list);
+		$scope.data.list = data.projects.filter(function(val) { 
+			if(val.active) { 
+				return val 
+			} 
+		});
 	});
+}])
+
+.directive('detailsDisplay', [function() {
+	return {
+		restrict: 'E',
+		scope: {
+			projectUrl: '=',
+			projectImage: '=',
+			projectDesc: '=',
+			projectPrev: '=',
+			projectNext: '=',
+			projectList: '='
+		},
+		replace: true,
+		link: function(scope, elm, attrs) {
+			scope.clicked = function(data) {
+				var prev = scope.projectList[data.prev],
+					next = scope.projectList[data.next];
+
+				if(data.direction === 'prev') {
+					scope.projectImage = '/assets/images/' + prev.img;
+					scope.projectDesc = prev.desc;
+					scope.projectUrl = prev.str;
+					scope.projectPrev = restrictPrev(data.prev - 1);
+					scope.projectNext = restrictPrev(data.next - 1);
+				}
+
+				if(data.direction === 'next') {
+					scope.projectImage = '/assets/images/' + next.img;
+					scope.projectDesc = next.desc;
+					scope.projectUrl = next.str;
+					scope.projectPrev = restrictNext(data.prev + 1);
+					scope.projectNext = restrictNext(data.next + 1);
+				}
+			};
+
+			var restrictPrev = function(val) {
+				return (val) === -1 ? scope.projectList.length - 1 : val;
+			};
+
+			var restrictNext = function(val) {
+				return (val) === scope.projectList.length ? 0 : val;
+			};
+		},
+		template:
+			'<div>'+
+				'<i class="fa fa-chevron-left" ng-click="clicked({str: projectUrl, img: projectImage, desc: projectDesc, prev: projectPrev, next: projectNext, direction: \'prev\'})"></i>'+
+				'<img ng-src="{{projectImage}}">'+
+				'<span>{{projectDesc}} <a ng-href="{{projectUrl}}" target="_blank">[open in new tab]</a></span>'+
+				'<i class="fa fa-chevron-right" ng-click="clicked({str: projectUrl, img: projectImage, desc: projectDesc, prev: projectPrev, next: projectNext, direction: \'next\'})"></i>'+
+			'</div>'
+	}
 }]);
